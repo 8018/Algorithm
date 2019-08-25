06048e9ee916e65c1d621dde8acdec47ba1ea02a
Neo <liuxiangfei29@gmail.com>
2019-08-25 22:19:31 +0800

    ha

--- src/me/xfly/algorithm/flashback/EightQueens.java
+++ src/me/xfly/algorithm/flashback/EightQueens.java
@@ -5,9 +5,10 @@ public class EightQueens {
 		new EightQueens().cal8queens(0);
 	}
 
-	int[] result = new int[8];// 全局或成员变量, 下标表示行, 值表示 queen 存储在哪一列
+	 int index = 0;
+	 int[] result = new int[8];// 全局或成员变量, 下标表示行, 值表示 queen 存储在哪一列
 
-	public void cal8queens(int row) { // 调用方式：cal8queens(0);
+	public  void cal8queens(int row) { // 调用方式：cal8queens(0);
 		if (row == 8) { // 8 个棋子都放置好了，打印结果
 			printQueens(result);
 			return; // 8 行棋子都放好了，已经没法再往下递归了，所以就 return
@@ -20,7 +21,7 @@ public class EightQueens {
 		}
 	}
 
-	private boolean isOk(int row, int column) {// 判断 row 行 column 列放置是否合适
+	private  boolean isOk(int row, int column) {// 判断 row 行 column 列放置是否合适
 		int leftup = column - 1, rightup = column + 1;
 		for (int i = row - 1; i >= 0; --i) { // 逐行往上考察每一行
 			if (result[i] == column)
@@ -39,7 +40,7 @@ public class EightQueens {
 		return true;
 	}
 
-	private void printQueens(int[] result) { // 打印出一个二维矩阵
+	private  void printQueens(int[] result) { // 打印出一个二维矩阵
 		for (int row = 0; row < 8; ++row) {
 			for (int column = 0; column < 8; ++column) {
 				if (result[row] == column)
@@ -49,7 +50,7 @@ public class EightQueens {
 			}
 			System.out.println();
 		}
-		System.out.println();
+		System.out.println(index++);
 	}
 
 }
