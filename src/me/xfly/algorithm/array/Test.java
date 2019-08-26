1ec22ea4afaf1a64f734ff3f486eabca237cd96d
Neo <liuxiangfei29@gmail.com>
2019-08-26 17:09:38 +0800

    删除测试代码

--- src/me/xfly/algorithm/array/Test.java
+++ src/me/xfly/algorithm/array/Test.java
@@ -111,36 +111,7 @@ public class Test {
 		return ans;
 	}
 
-	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-		ListNode resultHead = new ListNode(0);
-		ListNode p = l1, q = l2, curr = resultHead;
-		//进位
-		int carry = 0;
-		while (p != null || q != null) {
-			int x = p != null ? p.val : 0;
-			int y = q != null ? q.val : 0;
-			int value = x + y + carry;
-			//进位
-			carry = value / 10;
-			//当前位的实际数值
-			curr.next = new ListNode(value % 10);
-			curr = curr.next;
-			if (p != null) {
-				p = p.next;
-			}
-			if (q != null) {
-				q = q.next;
-			}
-
-		}
-		//如果最后仍有进位，用一个节点保存进位
-		if (carry != 0) {
-			curr.next = new ListNode(carry);
-		}
-
-		return resultHead.next;
-
-	}
+	
 
 	public int[] twoSum(int[] nums, int target) {
 
@@ -211,11 +182,4 @@ public class Test {
 	}
 }
 
-class ListNode {
-	int val;
-	ListNode next;
 
-	ListNode(int x) {
-		val = x;
-	}
-}
