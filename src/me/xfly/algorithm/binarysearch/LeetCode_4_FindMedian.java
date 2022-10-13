d755a2913778ae2d482d331a306e98eaec58c6e7
Xiangfei.Liu2 <Xiangfei.Liu2@zeekrlife.com>
2022-10-13 13:57:31 +0800

    doc:两个数组的中位数添加注释

--- src/me/xfly/algorithm/binarysearch/LeetCode_4_FindMedian.java
+++ src/me/xfly/algorithm/binarysearch/LeetCode_4_FindMedian.java
@@ -3,32 +3,34 @@ package me.xfly.algorithm.binarysearch;
 public class LeetCode_4_FindMedian {
 
     public static void main(String[] args) {
-        int[] nums1 = {2, 3, 4};
-        int[] nums2 = {1, 3, 5};
+        int[] nums1 = {1, 3};
+        int[] nums2 = {};
         System.out.println(findMedianSortedArrays(nums1, nums2));
     }
 
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int mLength = nums1.length;
-        int nLength = nums2.length;
-        int left = (mLength + nLength + 1) / 2;
-        int right = (mLength + nLength + 2) / 2;
-        return (findKthSmallestNum(nums1, 0, mLength - 1, nums2, 0, nLength - 1, left) +
-                findKthSmallestNum(nums1, 0, mLength - 1, nums2, 0, nLength - 1, right)) / 2.0d;
+        int length1 = nums1.length;
+        int length2 = nums2.length;
+
+        //两个数组长度和如果为偶数，则中位数是中间两个数的平均值，因此要找到两个数
+        int mid1 = (length1 + length2 + 1) / 2;
+        int mid2 = (length1 + length2 + 2) / 2;
+
+        return (findKthSmallestNum(nums1, 0, length1 - 1, nums2, 0, length2 - 1, mid1) + findKthSmallestNum(nums1, 0, length1 - 1, nums2, 0, length2 - 1, mid2)) / 2.0d;
     }
 
-    private static double findKthSmallestNum(int[] nums1, int start1, int end1, int[] nums2, int start2, int end2, int k) {
-        //实际长度是尾标减去首标加一
-        int mLength = end1 - start1 + 1;
-        int nLength = end2 - start2 + 1;
+    public static double findKthSmallestNum(int[] nums1, int start1, int end1, int[] nums2, int start2, int end2, int k) {
+        //注意此处不是数组长度，而是可用数据的个数
+        int length1 = end1 - start1 + 1;
+        int length2 = end2 - start2 + 1;
 
-        //调整统一把短的数组放在前面
-        if (mLength > nLength) {
+        //统一转换成第一额数组长度比第二个数组短，方便后续操作
+        if (length1 > length2) {
             return findKthSmallestNum(nums2, start2, end2, nums1, start1, end1, k);
         }
 
-        //如果短的数组已经到头，直接返回长的那个
-        if (mLength == 0) {
+        //一个数组为空或所有数据都被用到，直接从另一个返回
+        if (length1 == 0) {
             return nums2[start2 + k - 1];
         }
 
@@ -36,13 +38,14 @@ public class LeetCode_4_FindMedian {
             return Math.min(nums1[start1], nums2[start2]);
         }
 
-        //找出两个数组的前 k/2 位对比，如果一个小于另一个，则移除全部数据
-        int index1 = start1 + Math.min(mLength, k / 2) - 1;
-        int index2 = start2 + Math.min(nLength, k / 2) - 1;
+        //每个数组取 k/2 个数对比，小的那边全部淘汰
+        int index1 = start1 + Math.min(length1, k / 2) - 1;
+        int index2 = start2 + Math.min(length2, k / 2) - 1;
 
         if (nums1[index1] < nums2[index2]) {
+            //输的那边全部淘汰，因此下次的起始 index 为 index1 + 1
+            //k 为 k 减去淘汰的个数 （index1 - start1）+ 1, 比如 index1 = 1，start1 = 1，实际是淘汰一个，所以要加一
             return findKthSmallestNum(nums1, index1 + 1, end1, nums2, start2, end2, k - (index1 - start1 + 1));
-
         } else {
             return findKthSmallestNum(nums1, start1, end1, nums2, index2 + 1, end2, k - (index2 - start2 + 1));
         }
